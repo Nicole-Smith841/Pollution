ef066528fd9f4b861313b4f01448ad8091ca063a
Nicole-Smith841 <nicole.smith630@gmail.com>
2025-06-21 21:04:42 -0600

    Add files via upload

--- R Final Project.R
+++ R Final Project.R
@@ -6,6 +6,9 @@ Pollution_clean <- Pollution %>%
     Population.Not.Staying.at.Home = as.numeric(gsub(",", "", Population.Not.Staying.at.Home))
   )
 
+#This is quite the amount of data, so we are going to group the data a couple of different ways.
+
+
 state_summary <- Pollution_clean %>%
   group_by(State) %>%
   summarize(
@@ -23,7 +26,10 @@ city_summary <- Pollution_clean %>%
 
 sd(Pollution_clean$mil_miles)
 
-Z <- (46.857 - 22.18) / (52.9639 / sqrt(55))
+z_scores <- (x - mu) / sigma
+#performing a Z-score test to see if there is a correlation between miles and the CO3 
+#emissions in the air, the test came back as 3.4 which means that there is quite the difference
+#in standard deviation between those two variables. 
 
 ggplot(Pollution_clean, aes(x = Population.Not.Staying.at.Home, y = no2_median)) +
   geom_point() +
@@ -40,3 +46,10 @@ ggplot(state_summary, aes(x = State, y = total_not_staying_home)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   labs(title = "People going out in Each State", x = "States", y = "Going Out") +
   theme_minimal()
+
+#By looking at the graphs, we can see that the scatter plot shows that there are some 
+#states that average quite a few miles  (CA, IL, AZ) but the rest are in a group between 13 and
+#75 million miles. The bar graph corroborates that with people going out in those same states. 
+#This would support the hypothesis that the more people go out the more air pollution there is.
+#However, with the linear regression model it would appear that there is a weak linear relationship 
+#between staying at home and the N02 levels in the air. 
